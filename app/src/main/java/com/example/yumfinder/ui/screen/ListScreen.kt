dce08d94d22ed60bed2cf80ee98317aa8adc84b6
Charlie Apolinsky <charlie.apolinsky@gmail.com>
2024-10-30 16:02:48 +0100

    [Init] Initializing Repo

--- app/src/main/java/com/example/yumfinder/ui/screen/ListScreen.kt
+++ app/src/main/java/com/example/yumfinder/ui/screen/ListScreen.kt
@@ -178,7 +178,7 @@ fun AddRestaurantDialog (
                         }
                     }
                 ) {
-                    Text(text = "Add Restaurant")
+                    Text(text = "Add Restaurants")
                 }
             }
 
